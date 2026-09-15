134ca7424afa81bd9e6a4c7d7b683b15d761df97
segadx <segadesantiago7@gmail.com>
2026-09-14 21:39:55 -0300

    correccion tests de subCircuitoMásResistente y formateo

--- TP1.hs
+++ TP1.hs
@@ -1,54 +1,70 @@
 module TP1 where
 
 data Caja = Bombilla Bool | Nada
-              deriving Eq
-instance Show Caja where
-    show = showDeCaja
+  deriving (Eq)
 
-showDeCaja :: Caja -> String 
+instance Show Caja where
+  show = showDeCaja
+
+showDeCaja :: Caja -> String
 showDeCaja (Bombilla True) = "💡"
 showDeCaja (Bombilla False) = "⚪️"
 showDeCaja (Nada) = "🛑"
 
-data Circuito = Caja     Caja
-              | Serie    Circuito Circuito
-              | Paralelo Caja Circuito Circuito Caja
-                  deriving Eq
+data Circuito
+  = Caja Caja
+  | Serie Circuito Circuito
+  | Paralelo Caja Circuito Circuito Caja
+  deriving (Eq)
+
 instance Show Circuito where
-    show = showDeCircuito
+  show = showDeCircuito
 
 showDeCircuito :: Circuito -> String
 showDeCircuito (Caja caja) = showDeCaja caja
 showDeCircuito (Serie circuitoInicial circuitoFinal) =
   (showDeCircuito circuitoInicial) ++ "-" ++ (showDeCircuito circuitoFinal)
 showDeCircuito (Paralelo cajaEntrada circuitoIzquierdo circuitoDerecho cajaSalida) =
-  (showDeCaja cajaEntrada) ++
-  "{" ++ (showDeCircuito circuitoIzquierdo) ++ "}" ++
-  "{" ++ (showDeCircuito circuitoDerecho) ++ "}" ++
-  (showDeCaja cajaSalida)
+  (showDeCaja cajaEntrada)
+    ++ "{"
+    ++ (showDeCircuito circuitoIzquierdo)
+    ++ "}"
+    ++ "{"
+    ++ (showDeCircuito circuitoDerecho)
+    ++ "}"
+    ++ (showDeCaja cajaSalida)
 
 showDeCircuitoConEstructura :: Circuito -> String
 showDeCircuitoConEstructura (Caja caja) = showDeCaja caja
-showDeCircuitoConEstructura (Serie circuitoInicial circuitoFinal) = "(" ++
-  (showDeCircuitoConEstructura circuitoInicial) ++
-    "-" ++
-  (showDeCircuitoConEstructura circuitoFinal) ++ ")"
+showDeCircuitoConEstructura (Serie circuitoInicial circuitoFinal) =
+  "("
+    ++ (showDeCircuitoConEstructura circuitoInicial)
+    ++ "-"
+    ++ (showDeCircuitoConEstructura circuitoFinal)
+    ++ ")"
 showDeCircuitoConEstructura (Paralelo cajaEntrada circuitoIzquierdo circuitoDerecho cajaSalida) =
-  (showDeCaja cajaEntrada) ++
-  "{" ++ (showDeCircuitoConEstructura circuitoIzquierdo) ++ "}" ++
-  "{" ++ (showDeCircuitoConEstructura circuitoDerecho) ++ "}" ++
-  (showDeCaja cajaSalida)
+  (showDeCaja cajaEntrada)
+    ++ "{"
+    ++ (showDeCircuitoConEstructura circuitoIzquierdo)
+    ++ "}"
+    ++ "{"
+    ++ (showDeCircuitoConEstructura circuitoDerecho)
+    ++ "}"
+    ++ (showDeCaja cajaSalida)
 
-on  = Bombilla True
+on = Bombilla True
+
 off = Bombilla False
 
-cajaOn   = Caja on
-cajaOff  = Caja off
+cajaOn = Caja on
+
+cajaOff = Caja off
+
 cajaNada = Caja Nada
 
 -- 1: recCircuito
 
-recCircuito:: (Caja -> a) -> (Circuito -> Circuito -> a -> a -> a) -> (Caja -> Circuito -> Circuito -> Caja -> a -> a -> a -> a -> a) -> Circuito -> a
+recCircuito :: (Caja -> a) -> (Circuito -> Circuito -> a -> a -> a) -> (Caja -> Circuito -> Circuito -> Caja -> a -> a -> a -> a -> a) -> Circuito -> a
 recCircuito fCaja fSerie fParalelo circuito = case circuito of
   (Caja caja) -> fCaja caja
   (Serie circ1 circ2) -> fSerie circ1 circ2 (rec circ1) (rec circ2)
@@ -58,115 +74,131 @@ recCircuito fCaja fSerie fParalelo circuito = case circuito of
 
 -- 2: foldCircuito
 
-foldCircuito:: (Caja -> a) -> (a -> a -> a) -> (a -> a -> a -> a -> a) -> Circuito -> a
-foldCircuito fCaja fSerie fParalelo = recCircuito
-  fCaja
-  (\_ _ recCirc1 recCirc2 -> fSerie recCirc1 recCirc2)
-  (\_ _ _ _ recCaja1 recCirc1 recCirc2 recCaja2 -> fParalelo recCaja1 recCirc1 recCirc2 recCaja2)
+foldCircuito :: (Caja -> a) -> (a -> a -> a) -> (a -> a -> a -> a -> a) -> Circuito -> a
+foldCircuito fCaja fSerie fParalelo =
+  recCircuito
+    fCaja
+    (\_ _ recCirc1 recCirc2 -> fSerie recCirc1 recCirc2)
+    (\_ _ _ _ recCaja1 recCirc1 recCirc2 recCaja2 -> fParalelo recCaja1 recCirc1 recCirc2 recCaja2)
 
 -- 3 invertido
 
-invertido:: Circuito -> Circuito
-invertido = foldCircuito
-  Caja
-  (flip Serie)
-  (\(Caja caja1) circ1 circ2 (Caja caja2) -> Paralelo caja2 circ2 circ1 caja1)
+invertido :: Circuito -> Circuito
+invertido =
+  foldCircuito
+    Caja
+    (flip Serie)
+    (\(Caja caja1) circ1 circ2 (Caja caja2) -> Paralelo caja2 circ2 circ1 caja1)
 
 -- 4: hayCaminoIluminado
 
-hayCaminoIluminado:: Circuito -> Bool
-hayCaminoIluminado = foldCircuito
-  (==on)
-  (&&)
-  (\caja1 circ1 circ2 caja2 -> caja1&&(circ1||circ2)&&caja2)
+hayCaminoIluminado :: Circuito -> Bool
+hayCaminoIluminado =
+  foldCircuito
+    (== on)
+    (&&)
+    (\caja1 circ1 circ2 caja2 -> caja1 && (circ1 || circ2) && caja2)
 
 -- 5: cantidadPrendidas
 
-cantidadPrendidas:: Circuito -> Int
-cantidadPrendidas = foldCircuito
-  (\caja -> if caja==on then 1 else 0)
-  (+)
-  (\caja1 circ1 circ2 caja2 -> caja1+circ1+circ2+caja2)
+cantidadPrendidas :: Circuito -> Int
+cantidadPrendidas =
+  foldCircuito
+    (\caja -> if caja == on then 1 else 0)
+    (+)
+    (\caja1 circ1 circ2 caja2 -> caja1 + circ1 + circ2 + caja2)
 
 -- 6: cajasDeCircuito
 
-cajasDeCircuito:: Circuito -> [Caja]
-cajasDeCircuito = foldCircuito
-  (: [])
-  (++)
-  (\caja1 circ1 circ2 caja2 -> caja1++circ1++circ2++caja2)
+cajasDeCircuito :: Circuito -> [Caja]
+cajasDeCircuito =
+  foldCircuito
+    (: [])
+    (++)
+    (\caja1 circ1 circ2 caja2 -> caja1 ++ circ1 ++ circ2 ++ caja2)
 
 -- 7: esCircuitoProlijo
 
-esCircuitoProlijo:: Circuito -> Bool
-esCircuitoProlijo = recCircuito
-  (const True)
-  (\_ circ2 recCirc1 recCirc2 -> case circ2 of
-    (Caja _) -> recCirc1
-    (Serie _ _) -> False
-    (Paralelo _ _ _ _) -> recCirc1&&recCirc2
-  )
-  (\_ _ _ _ _ _ _ _ -> True)
+esCircuitoProlijo :: Circuito -> Bool
+esCircuitoProlijo =
+  recCircuito
+    (const True)
+    ( \_ circ2 recCirc1 recCirc2 -> case circ2 of
+        (Caja _) -> recCirc1
+        (Serie _ _) -> False
+        (Paralelo _ _ _ _) -> recCirc1 && recCirc2
+    )
+    (\_ _ _ _ _ _ _ _ -> True)
 
 -- 8: circuitoEmprolijado
 
-circuitoEmprolijado:: Circuito -> Circuito
-circuitoEmprolijado = foldCircuito
-  Caja
-  (\recCirc1 recCirc2 -> case recCirc2 of
-    Caja caja -> Serie recCirc1 recCirc2
-    Serie c1 c2 -> Serie (Serie recCirc1 c1) c2
-    Paralelo _ _ _ _ -> Serie recCirc1 recCirc2
-  )
-  (\(Caja recCaja1) recCirc1 recCirc2 (Caja recCaja2) -> Paralelo recCaja1 recCirc1 recCirc2 recCaja2)
+circuitoEmprolijado :: Circuito -> Circuito
+circuitoEmprolijado =
+  foldCircuito
+    Caja
+    ( \recCirc1 recCirc2 -> case recCirc2 of
+        Caja caja -> Serie recCirc1 recCirc2
+        Serie c1 c2 -> Serie (Serie recCirc1 c1) c2
+        Paralelo _ _ _ _ -> Serie recCirc1 recCirc2
+    )
+    (\(Caja recCaja1) recCirc1 recCirc2 (Caja recCaja2) -> Paralelo recCaja1 recCirc1 recCirc2 recCaja2)
 
--- 9: tienenLaMismaEstructura 
+-- 9: tienenLaMismaEstructura
 
-tienenLaMismaEstructura:: Circuito -> Circuito -> Bool
-tienenLaMismaEstructura = foldCircuito
-  (\cajaCirc1 circ -> case circ of
-    Caja _ -> True
-    Serie _ _ -> False
-    Paralelo _ _ _ _ -> False
-  )
-  (\recCircuito1 recCircuito2 circ -> case circ of
-    Caja _ -> False
-    Serie c1 c2 -> recCircuito1 c1 && recCircuito2 c2
-    Paralelo _ _ _ _ -> False
-  )
-  (\_ recCirc1 recCirc2 _ circ -> case circ of 
-    Caja _ -> False
-    Serie _ _ -> False
-    Paralelo _ c1 c2 _ -> recCirc1 c1 && recCirc2 c2
-  )
+tienenLaMismaEstructura :: Circuito -> Circuito -> Bool
+tienenLaMismaEstructura =
+  foldCircuito
+    ( \cajaCirc1 circ -> case circ of
+        Caja _ -> True
+        Serie _ _ -> False
+        Paralelo _ _ _ _ -> False
+    )
+    ( \recCircuito1 recCircuito2 circ -> case circ of
+        Caja _ -> False
+        Serie c1 c2 -> recCircuito1 c1 && recCircuito2 c2
+        Paralelo _ _ _ _ -> False
+    )
+    ( \_ recCirc1 recCirc2 _ circ -> case circ of
+        Caja _ -> False
+        Serie _ _ -> False
+        Paralelo _ c1 c2 _ -> recCirc1 c1 && recCirc2 c2
+    )
 
 -- 10: subCircuitoMásResistente
 
-resistenciaCircuito:: Circuito -> Float
-resistenciaCircuito = foldCircuito
-  (\_ -> 1.0) 
-  (+)
-  (\rCajaE rC1 rC2 rCajaS -> rCajaE + ((rC1 * rC2) / (rC1 + rC2)) + rCajaS)
+resistenciaCircuito :: Circuito -> Float
+resistenciaCircuito =
+  foldCircuito
+    (\_ -> 1.0)
+    (+)
+    (\rCajaE rC1 rC2 rCajaS -> rCajaE + ((rC1 * rC2) / (rC1 + rC2)) + rCajaS)
 
-subCircuitoMásResistente:: Circuito -> Circuito
-subCircuitoMásResistente = recCircuito
-  Caja
-  (\circ1 circ2 recCirc1 recCirc2 -> if resistenciaCircuito recCirc1 > resistenciaCircuito recCirc2
-    then if resistenciaCircuito (Serie circ1 circ2) > resistenciaCircuito recCirc1
-      then Serie circ1 circ2
-      else recCirc1
-    else if resistenciaCircuito (Serie circ1 circ2) > resistenciaCircuito recCirc2
-      then Serie circ1 circ2
-      else recCirc2
-  )
-  (\caja1 circ1 circ2 caja2 _ recCirc1 recCirc2 _ -> if resistenciaCircuito recCirc1 > resistenciaCircuito recCirc2
-    then if resistenciaCircuito (Paralelo caja1 circ1 circ2 caja2) > resistenciaCircuito recCirc1
-      then Paralelo caja1 circ1 circ2 caja2
-      else recCirc1
-    else if resistenciaCircuito (Paralelo caja1 circ1 circ2 caja2) > resistenciaCircuito recCirc2
-      then Paralelo caja1 circ1 circ2 caja2
-      else recCirc2
-  )
+subCircuitoMásResistente :: Circuito -> Circuito
+subCircuitoMásResistente =
+  recCircuito
+    Caja
+    ( \circ1 circ2 recCirc1 recCirc2 ->
+        if resistenciaCircuito recCirc1 > resistenciaCircuito recCirc2
+          then
+            if resistenciaCircuito (Serie circ1 circ2) > resistenciaCircuito recCirc1
+              then Serie circ1 circ2
+              else recCirc1
+          else
+            if resistenciaCircuito (Serie circ1 circ2) > resistenciaCircuito recCirc2
+              then Serie circ1 circ2
+              else recCirc2
+    )
+    ( \caja1 circ1 circ2 caja2 _ recCirc1 recCirc2 _ ->
+        if resistenciaCircuito recCirc1 > resistenciaCircuito recCirc2
+          then
+            if resistenciaCircuito (Paralelo caja1 circ1 circ2 caja2) > resistenciaCircuito recCirc1
+              then Paralelo caja1 circ1 circ2 caja2
+              else recCirc1
+          else
+            if resistenciaCircuito (Paralelo caja1 circ1 circ2 caja2) > resistenciaCircuito recCirc2
+              then Paralelo caja1 circ1 circ2 caja2
+              else recCirc2
+    )
 
 {-- 11: Demostrar: alternado . alternado = id
 
@@ -189,8 +221,6 @@ id :: a -> a
 not :: Bool -> Bool
 {NT} not True = False
 {NF} not False = True
-
-
 
 Por Principio de Extensionalidad Funcional, basta probar que
     (∀ circ:: Circuito) alternado . alternado circ = id circ
@@ -215,7 +245,7 @@ Por Principio de Inducción sobre Circuitos, basta probar los siguientes casos:
       = Serie (id circ1) (id circ2)                                       {HI}
       = Serie circ1 circ2                                                 {I}
       = id (Serie circ1 circ2)                                            {I}
-    
+
     3) Paso Inductivo Paralelo: Debo probar que (∀ caja1:: Caja) (∀ circ1:: Circuito) (∀ circ2:: Circuito) (∀ caja2:: Caja) ((P(circ1) ∧ P(circ2)) → P(Paralelo caja1 circ1 circ2 caja2))
       alternado . alternado (Paralelo caja1 circ1 circ2 caja2)
       = alternado (alternado (Paralelo caja1 circ1 circ2 caja2))                                                                                          {C}
@@ -228,7 +258,6 @@ Por Principio de Inducción sobre Circuitos, basta probar los siguientes casos:
       = id (Paralelo caja1 circ1 circ2 caja2)                                                                                                             {I}
 
 Luego alternado . alternado = id
-
 
 - Prueba de LEMA: Debo probar que
   (∀ caja:: Caja) (cajaAlternada (cajaAlternada caja)) = caja
--- tests.hs
+++ tests.hs
@@ -37,109 +37,121 @@ testsInvertido =
     ]
 
 testsHayCaminoIluminado :: Test
-testsHayCaminoIluminado = TestList -- TODO: AGREGAR
-  [ "En una caja con bombilla encendida hay camino iluminado"
-    ~: hayCaminoIluminado cajaOn
-    ~?= True
-  , "Hay Camino Iluminado (2)"
-    ~: hayCaminoIluminado (Serie
-      (Paralelo
-        on
-        (Paralelo off cajaNada cajaOn on)
-        (Paralelo Nada cajaOn cajaOff Nada)
-        on
-      )
-      cajaOn
-    )
-    ~?= False
-    , "Hay Camino Iluminado (3)"
-    ~: hayCaminoIluminado (Serie
-      (Paralelo
-        on
-        (Paralelo on cajaNada cajaOn on)
-        (Paralelo Nada cajaOn cajaOff Nada)
-        on
-      )
-      cajaOn
-    )
-    ~?= True
-    , "Hay Camino Iluminado (4)"
-    ~: hayCaminoIluminado (Serie
-      cajaOn
-      cajaOn
-    )
-    ~?= True
-  ]
+testsHayCaminoIluminado =
+  TestList -- TODO: AGREGAR
+    [ "En una caja con bombilla encendida hay camino iluminado"
+        ~: hayCaminoIluminado cajaOn
+        ~?= True,
+      "Hay Camino Iluminado (2)"
+        ~: hayCaminoIluminado
+          ( Serie
+              ( Paralelo
+                  on
+                  (Paralelo off cajaNada cajaOn on)
+                  (Paralelo Nada cajaOn cajaOff Nada)
+                  on
+              )
+              cajaOn
+          )
+        ~?= False,
+      "Hay Camino Iluminado (3)"
+        ~: hayCaminoIluminado
+          ( Serie
+              ( Paralelo
+                  on
+                  (Paralelo on cajaNada cajaOn on)
+                  (Paralelo Nada cajaOn cajaOff Nada)
+                  on
+              )
+              cajaOn
+          )
+        ~?= True,
+      "Hay Camino Iluminado (4)"
+        ~: hayCaminoIluminado
+          ( Serie
+              cajaOn
+              cajaOn
+          )
+        ~?= True
+    ]
 
 testsCantidadPrendidas :: Test
-testsCantidadPrendidas = TestList -- TODO: AGREGAR
-  [ "Cantidad prendidas en caja prendida es 1"
-    ~: cantidadPrendidas cajaOn
-    ~?= 1
-  , "Cantidad Prendidas (2)"
-    ~: cantidadPrendidas (Serie
-      (Paralelo
-        on
-        (Paralelo off cajaNada cajaOn on)
-        (Paralelo Nada cajaOn cajaOff Nada)
-        on
-      )
-      cajaOn
-    )
-    ~?= 6
-  , "Cantidad Prendidas (3)"
-     ~: cantidadPrendidas (Serie
-     cajaOff
-     cajaOff
-     )
-     ~?= 0
-  , "Cantidad Prendidas (4)"
-    ~: cantidadPrendidas (Serie
-      (Paralelo
-        on
-        (Paralelo on cajaOn cajaOn on)
-        (Paralelo on cajaOn cajaOn on)
-        on
-      )
-      cajaOn
-    )
-    ~?= 11
-  ]
+testsCantidadPrendidas =
+  TestList -- TODO: AGREGAR
+    [ "Cantidad prendidas en caja prendida es 1"
+        ~: cantidadPrendidas cajaOn
+        ~?= 1,
+      "Cantidad Prendidas (2)"
+        ~: cantidadPrendidas
+          ( Serie
+              ( Paralelo
+                  on
+                  (Paralelo off cajaNada cajaOn on)
+                  (Paralelo Nada cajaOn cajaOff Nada)
+                  on
+              )
+              cajaOn
+          )
+        ~?= 6,
+      "Cantidad Prendidas (3)"
+        ~: cantidadPrendidas
+          ( Serie
+              cajaOff
+              cajaOff
+          )
+        ~?= 0,
+      "Cantidad Prendidas (4)"
+        ~: cantidadPrendidas
+          ( Serie
+              ( Paralelo
+                  on
+                  (Paralelo on cajaOn cajaOn on)
+                  (Paralelo on cajaOn cajaOn on)
+                  on
+              )
+              cajaOn
+          )
+        ~?= 11
+    ]
 
 testsCajasDeCircuito :: Test
-testsCajasDeCircuito = TestList -- TODO: AGREGAR
-  [ "La lista de cajas de un circuito con una única caja es la lista con esa caja"
-    ~: cajasDeCircuito cajaOn
-    ~?= [on]
-  , "Cajas de Circuito (2)"
-    ~: cajasDeCircuito (Serie
-      (Paralelo
-        on
-        (Paralelo off cajaNada cajaOn on)
-        (Paralelo Nada cajaOn cajaOff Nada)
-        on
-      )
-      cajaOn
-    )
-    ~?= [on, off, Nada, on, on, Nada, on, off, Nada, on, on]
-  , "Cajas de Circuito (3)"
-    ~: cajasDeCircuito (Serie
-      (Paralelo
-        on
-        cajaOff
-        cajaOn
-        on
-      )
-        cajaOn
-      )
-    ~?= [on, off, on, on, on]
-    , "Cajas de Circuito (4)"
-    ~: cajasDeCircuito (Serie
-        cajaNada
-        cajaNada
-      )
-    ~?= [Nada, Nada]
-  ]
+testsCajasDeCircuito =
+  TestList -- TODO: AGREGAR
+    [ "La lista de cajas de un circuito con una única caja es la lista con esa caja"
+        ~: cajasDeCircuito cajaOn
+        ~?= [on],
+      "Cajas de Circuito (2)"
+        ~: cajasDeCircuito
+          ( Serie
+              ( Paralelo
+                  on
+                  (Paralelo off cajaNada cajaOn on)
+                  (Paralelo Nada cajaOn cajaOff Nada)
+                  on
+              )
+              cajaOn
+          )
+        ~?= [on, off, Nada, on, on, Nada, on, off, Nada, on, on],
+      "Cajas de Circuito (3)"
+        ~: cajasDeCircuito
+          ( Serie
+              ( Paralelo
+                  on
+                  cajaOff
+                  cajaOn
+                  on
+              )
+              cajaOn
+          )
+        ~?= [on, off, on, on, on],
+      "Cajas de Circuito (4)"
+        ~: cajasDeCircuito
+          ( Serie
+              cajaNada
+              cajaNada
+          )
+        ~?= [Nada, Nada]
+    ]
 
 testsEsCircuitoProlijo :: Test
 testsEsCircuitoProlijo =
@@ -173,59 +185,60 @@ testsCircuitoEmprolijado =
     ]
 
 testsTienenLaMismaEstructura :: Test
-testsTienenLaMismaEstructura = TestList
-  [ "Test Tienen La Misma Estructura (1)"
-    ~: tienenLaMismaEstructura
-      (Serie
-        cajaOn
-        (Paralelo
-        on
-        (Paralelo Nada cajaOff cajaOn Nada)
-        (Paralelo on cajaOn cajaNada off)
-        on
-        )
-      )
-      (Serie
-        cajaOff
-        (Paralelo
-        on
-        (Paralelo off cajaOff cajaOn on)
-        (Paralelo Nada cajaOff cajaNada Nada)
-        off
-        )
-      )
-    ~?= True
-  , "Test Tienen La Misma Estructura (2)"
-    ~: tienenLaMismaEstructura
-      (Serie
-        cajaOn
-        (Paralelo
-        on
-        (Paralelo Nada cajaOff cajaOn Nada)
-        (Paralelo on cajaOn cajaNada off)
-        on
-        )
-      )
-      (Serie
-        cajaOff
-        (Paralelo
-        on
-        (Serie cajaOff cajaOn)
-        (Paralelo Nada cajaOff cajaNada Nada)
-        off
-        )
-      )
-    ~?= False
-    , "Test Tienen La Misma Estructura (3)"
-    ~: tienenLaMismaEstructura
-      (Caja 
-        on
-      )
-      (Caja
-        off
-      )
-    ~?= True
-  ]
+testsTienenLaMismaEstructura =
+  TestList
+    [ "Test Tienen La Misma Estructura (1)"
+        ~: tienenLaMismaEstructura
+          ( Serie
+              cajaOn
+              ( Paralelo
+                  on
+                  (Paralelo Nada cajaOff cajaOn Nada)
+                  (Paralelo on cajaOn cajaNada off)
+                  on
+              )
+          )
+          ( Serie
+              cajaOff
+              ( Paralelo
+                  on
+                  (Paralelo off cajaOff cajaOn on)
+                  (Paralelo Nada cajaOff cajaNada Nada)
+                  off
+              )
+          )
+        ~?= True,
+      "Test Tienen La Misma Estructura (2)"
+        ~: tienenLaMismaEstructura
+          ( Serie
+              cajaOn
+              ( Paralelo
+                  on
+                  (Paralelo Nada cajaOff cajaOn Nada)
+                  (Paralelo on cajaOn cajaNada off)
+                  on
+              )
+          )
+          ( Serie
+              cajaOff
+              ( Paralelo
+                  on
+                  (Serie cajaOff cajaOn)
+                  (Paralelo Nada cajaOff cajaNada Nada)
+                  off
+              )
+          )
+        ~?= False,
+      "Test Tienen La Misma Estructura (3)"
+        ~: tienenLaMismaEstructura
+          ( Caja
+              on
+          )
+          ( Caja
+              off
+          )
+        ~?= True
+    ]
 
 -- En particular aca cajaOn y cajaOff tienen la misma resistencia (1), por lo tanto, es indistinto usar uno u otro (por como implementamos "resistenciaCircuito")
 testsSubCircuitoMásResistente :: Test
@@ -235,15 +248,25 @@ testsSubCircuitoMásResistente =
         ~: subCircuitoMásResistente cajaOn
         ~?= cajaOn,
       "En una serie, la serie completa es más resistente que las cajas individuales (1+1=2)"
-        ~: subCircuitoMásResistente (Serie cajaOn cajaOn)
+        ~: subCircuitoMásResistente
+          ( Serie
+              cajaOn
+              cajaOn
+          )
         ~?= Serie cajaOn cajaOn,
       "En un paralelo chico, el paralelo completo suma más (1 + 0.5 + 1 = 2.5 > 1)"
-        ~: subCircuitoMásResistente (Paralelo cajaOn cajaOn cajaOn cajaOn)
-        ~?= Paralelo cajaOn cajaOn cajaOn cajaOn,
+        ~: subCircuitoMásResistente
+          ( Paralelo
+              on
+              cajaOn
+              cajaOn
+              on
+          )
+        ~?= Paralelo on cajaOn cajaOn on,
       "Paralelo donde una rama es más resistente que el circuito total"
         ~: let ramaIzq = Serie (Serie cajaOn cajaOn) (Serie cajaOn (Serie cajaOn (Serie cajaOn cajaOn))) -- Resistencia 6
                ramaDer = Serie (Serie cajaOn cajaOn) (Serie cajaOn (Serie cajaOn cajaOn)) -- Resistencia 5
-            in subCircuitoMásResistente (Paralelo cajaOn ramaIzq ramaDer cajaOn)
+            in subCircuitoMásResistente (Paralelo on ramaIzq ramaDer on)
                  -- Resistencia Total = 1 + (6*5)/(6+5) + 1 = 1 + 2.7 + 1 = 4.7 aprox
                  -- Como 6 > 4.7, el subcircuito más resistente de todo el conjunto es la rama "ramaIzq"
                  ~?= ramaIzq
